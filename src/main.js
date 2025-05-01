92aae2ec0df36fe4014af44ab2e14d894f6ac053
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-05-01 10:29:24 +0200

    chamged the speed of earth

--- src/main.js
+++ src/main.js
@@ -190,7 +190,7 @@ function animateMoon(){
 
 function animateEarth(){
   requestAnimationFrame(animateEarth);
-  earthGroup.rotation.z += 0.00006;
+  earthGroup.rotation.z += 0.0001;
 
   renderer.render(scene, camera);
 }
